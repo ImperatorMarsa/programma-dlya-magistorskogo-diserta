bcaa19cce21f8ad0ab90ecbfcae6e6bc0690afaa
Тимофей Ситников <t.sitnikov@infocom-s.ru>
2019-07-20 20:42:14 +0300

    Добавил функцию заполнения матриц коректными данными

--- MathLib.py
+++ MathLib.py
@@ -50,6 +50,7 @@ Hy_amplitud = 7.3e3 #@param {type: "number"}
 
 Plotnost = 5000 # килограмм/метр^3
 
+Obyom_ = 4 / 3 * xp.pi * (Radiuse + Dlina_PAV) ** 3  # Метров^3
 Obyom = 4 / 3 * xp.pi * Radiuse ** 3  # Метров^3
 
 Massa = Obyom * Plotnost  # килограмм
@@ -58,7 +59,7 @@ NamagnicEdiniciObyoma = 4.78e5 #@param {type: "number"}
 
 # Метров
 Koncintraciya_obyomnaya = 0.10 #@param {type: "number"}
-GraniciVselennoy = math.pow(Obyom * CisloChastic / Koncintraciya_obyomnaya, 1/3)
+GraniciVselennoy = math.pow(Obyom_ * CisloChastic / Koncintraciya_obyomnaya, 1/3)
 
 #@markdown ---
 # <+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=<+>!=
@@ -126,218 +127,233 @@ def _SteerOttalk(matrix, uglVek, radVek):
 SteerOttalk = np.vectorize(_SteerOttalk, otypes=[float], signature='(m),(l),(k)->(k)')
 
 
+def createrChastic(koordi, namag, skor, uglSkor):
+    """Функция заполняющая матрицы для системы частиц
+    
+    Arguments:
+        koordi {array} -- Матрица векторов радиус координат
+        namag {array} -- Матрица векторов намагниченности
+        skor {array} -- Матрица векторов скоростей
+        uglSkor {array} -- Матрица векторов угловых скоростей
+    """
+    for i in range(len(koordi)):
+        # Задаю магнитный момент
+        rend_k = xp.random.rand(3) - 0.5
+        namag[i] = rend_k / xp.linalg.norm(rend_k) * MagMom(Radiuse)
 
-@jit(fastmath = True, nopython = True, parallel = True)
-def PrimoySila(mI, mJ, n, magN):
-    rIJ = (mI[RadVek] - mJ[RadVek]) + n
-    magR = math.sqrt(rIJ[0] ** 2 + rIJ[1] ** 2 + rIJ[2] ** 2)
-    mI[VekSil] += (
-        3
-        * U0
-        / (4 * xp.pi * magR ** 5)
-        * (
-            Dot(mI[NaprUgl], rIJ) * mJ[NaprUgl]
-            + Dot(mJ[NaprUgl], rIJ) * mI[NaprUgl]
-            + Dot(mI[NaprUgl], mJ[NaprUgl]) * rIJ
-            - 5 * Dot(mI[NaprUgl], rIJ) * Dot(mJ[NaprUgl], rIJ) * rIJ / magR ** 2
-        )
-    )
+        # Задаю наиболее вероятную скорость для частиц
+        rend_k = xp.random.rand(3) - 0.5
+        skor[i] = rend_k / xp.linalg.norm(rend_k) * xp.sqrt(2 * kT / Massa)
 
-    return mI
+        # Задаю наиболее вероятну угловую скорость частиц
+        rend_k = xp.random.rand(3) - 0.5
+        uglSkor[i] = rend_k / xp.linalg.norm(rend_k) * xp.sqrt(8 * kT / (2 / 5 *Massa * Radiuse**2))
 
+        # Устанавливаю координаты, так чтобы они небыли слишком близко
+        rend_k = xp.random.rand(3) * GraniciVselennoy
+        isTouch = True
+        _isTouch = True
+        j = 0
+        while isTouch:
+            koordi[j]
+            dist = xp.sqrt(xp.sum(xp.square(xp.copy(rend_k - koordi[j]))))
+            if dist < ((Radiuse + Dlina_PAV) * 2):
+                _isTouch = True
+            else:
+                _isTouch = False
 
-@jit(fastmath = True, nopython = True, parallel = True)
-def PrimoyMoment(mI, mJ, n, magN):
-    rIJ = (mI[RadVek] - mJ[RadVek]) + n
-    magR = math.sqrt(rIJ[0] ** 2 + rIJ[1] ** 2 + rIJ[2] ** 2)
+            j += 1
+            if j > i:
+                j = 0
+                if _isTouch:
+                    isTouch = True
+                    rend_k = xp.random.rand(3) * GraniciVselennoy
+                else:
+                    isTouch = False
+                    koordi[i] = rend_k
 
-    B_I = (
-        U0
-        / (4 * xp.pi)
-        * (Dot(mI[NaprUgl], rIJ) * 3 * rIJ / (magR) ** 5 - mI[NaprUgl] / (magR) ** 3)
-    )
-    mI[VekMomentov] += Cross(mJ[NaprUgl], B_I)
-
-    return mI
-
-
-# https://www.desmos.com/calculator/bhjmf8p0pf
-@jit(fastmath = True, nopython = True, parallel = True)
-def Kinematika(C):
-    pom = StahostSmeshLineynoe(C[ParamCastic][R_Chastici])
-    C[VecSkor] = C[RadVek]
-    C[RadVek] = (
-        C[RadVek]
-        + C[VekSil] / (6.0 * xp.pi * C[ParamCastic][R_Chastici] * Vyazkost) * delta_T
-        + pom
-    )
-    C[VecSkor] = (C[RadVek] - C[VecSkor]) / delta_T
-
-    pom = StahostSmeshUglovoe(C[ParamCastic][R_Chastici])
-    C[VekVrash] = C[NaprUgl]
-    DeltaAlfa = (
-        C[VekMomentov]
-        / (8.0 * xp.pi * C[ParamCastic][R_Chastici] ** 3 * Vyazkost)
-        * delta_T
-        + pom
-    )  # xp.linalg.norm(DeltaAlfa) #
-    buf = math.sqrt(DeltaAlfa[0] ** 2 + DeltaAlfa[1] ** 2 + DeltaAlfa[2] ** 2)
-    C[NaprUgl] = RotatinVec(C[NaprUgl], DeltaAlfa, buf)
-    C[VekVrash] = (C[NaprUgl] - C[VekVrash]) / delta_T
-
-    C = PorvrkaGrani(C)
-    C[VekSil] = xp.zeros(3)
-    C[VekMomentov] = xp.zeros(3)
-
-    return C
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def PorvrkaGrani(mass):
-    if mass[0][0] > GraniciVselennoy:
-        mass[0] = xp.array([mass[0][0] - 2 * GraniciVselennoy, mass[0][1], mass[0][2]])
-    elif mass[0][0] < -GraniciVselennoy:
-        mass[0] = xp.array([mass[0][0] + 2 * GraniciVselennoy, mass[0][1], mass[0][2]])
-
-    if mass[0][1] > GraniciVselennoy:
-        mass[0] = xp.array([mass[0][0], mass[0][1] - 2 * GraniciVselennoy, mass[0][2]])
-    elif mass[0][1] < -GraniciVselennoy:
-        mass[0] = xp.array([mass[0][0], mass[0][1] + 2 * GraniciVselennoy, mass[0][2]])
-
-    if mass[0][2] > GraniciVselennoy:
-        mass[0] = xp.array([mass[0][0], mass[0][1], mass[0][2] - 2 * GraniciVselennoy])
-    elif mass[0][2] < -GraniciVselennoy:
-        mass[0] = xp.array([mass[0][0], mass[0][1], mass[0][2] + 2 * GraniciVselennoy])
-
-    return mass
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def StahostSmeshLineynoe(Radiuse):
-    difuz = kT / (6.0 * xp.pi * Radiuse * Vyazkost)
-
-    return RandNormVec() * ((2 * difuz * delta_T) ** 0.5 * gauss(0, 1))
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def StahostSmeshUglovoe(Radiuse):
-    difuz = kT / (8.0 * xp.pi * Radiuse ** 3 * Vyazkost)
-
-    return RandNormVec() * ((2 * difuz * delta_T) ** 0.5 * gauss(0, 1))
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def GeneralLoop(mass):
-    for x in range(len(mass)):
-        mass[x] = Kinematika(mass[x])
-
-    return mass
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def MathKernel(mass, N):
-    for i in range(len(mass)):
-        mass[i] = VneshPole(mass[i], N)
-        for j in range(len(mass)):
-            mass[i] = SteerOttalk(mass[i], mass[j])
-
-            PredelSumm = 3
-            for X in range(-PredelSumm, PredelSumm + 1):
-                pom1 = int(math.sqrt(PredelSumm ** 2 - X ** 2))
-                for Y in range(-pom1, pom1 + 1):
-                    pom2 = int(math.sqrt(PredelSumm ** 2 - X ** 2 - Y ** 2))
-                    for Z in range(-pom2, pom2 + 1):
-                        n = xp.array(
-                            [
-                                X * GraniciVselennoy * 2,
-                                Y * GraniciVselennoy * 2,
-                                Z * GraniciVselennoy * 2,
-                            ]
-                        )
-                        magN = math.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
-
-                        if j != i:
-                            mass[i] = PrimoySila(mass[i], mass[j], n, magN)
-                            mass[i] = PrimoyMoment(mass[i], mass[j], n, magN)
-
-    return mass
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def Dot(a, b):
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
-
-
-@jit(fastmath = True, nopython = True, parallel = True)
-def RandNormVec():
-    a1 = 1 - 2 * random()
-    a2 = 1 - 2 * random()
-    a3 = 1 - 2 * random()
-    sq = math.sqrt(a1 ** 2 + a2 ** 2 + a3 ** 2)
-
-    return xp.array([a1 / sq, a2 / sq, a3 / sq])
+    return koordi, namag, skor, uglSkor
 
 
 @jit(fastmath = True, nopython = True, parallel = True)
 def Culculete(pom):
-    return xp.sqrt(xp.sum(xp.square(xp.copy(pom.reshape(len(pom), 7 * 3)[:, NaprUgl * 3:NaprUgl * 3 + 3])), axis = 1)) * MagMom
+    """Функция расчитываюшая среднее значение модуля для матрицы векторов
+    
+    Arguments:
+        pom {array} -- Матрицы векторов
+    """
+    return xp.sum(xp.sqrt(xp.sum(xp.square(xp.copy(pom)), axis = 1)))
 
 
-@jit(fastmath = True, nopython = True, parallel = True)
-def RotatinVec(vec, axis, ugol):
-    nK = math.sqrt(axis[0] ** 2 + axis[2] ** 2 + axis[1] ** 2)
-    axis[0], axis[1], axis[2] = axis[0] / nK, axis[1] / nK, axis[2] / nK
-    a = math.cos(ugol / 2.0)
-    sinKoef = math.sin(ugol / 2.0)
-    b, c, d = -axis[0] * sinKoef, -axis[1] * sinKoef, -axis[2] * sinKoef
-    aa, bb, cc, dd = a * a, b * b, c * c, d * d
-    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
-    q1, q2, q3 = aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)
-    q4, q5, q6 = 2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)
-    q7, q8, q9 = 2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc
-    p0 = q1 * vec[0] + q2 * vec[1] + q3 * vec[2]
-    p1 = q4 * vec[0] + q5 * vec[1] + q6 * vec[2]
-    p2 = q7 * vec[0] + q8 * vec[1] + q9 * vec[2]
-    return xp.array([p0, p1, p2])
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def PrimoySila(mI, mJ, n, magN):
+#     rIJ = (mI[RadVek] - mJ[RadVek]) + n
+#     magR = math.sqrt(rIJ[0] ** 2 + rIJ[1] ** 2 + rIJ[2] ** 2)
+#     mI[VekSil] += (
+#         3
+#         * U0
+#         / (4 * xp.pi * magR ** 5)
+#         * (
+#             Dot(mI[NaprUgl], rIJ) * mJ[NaprUgl]
+#             + Dot(mJ[NaprUgl], rIJ) * mI[NaprUgl]
+#             + Dot(mI[NaprUgl], mJ[NaprUgl]) * rIJ
+#             - 5 * Dot(mI[NaprUgl], rIJ) * Dot(mJ[NaprUgl], rIJ) * rIJ / magR ** 2
+#         )
+#     )
 
-def createrChastic(CisloChastic):
-    pom = [[]]
-    pom[-1].append(2 * GraniciVselennoy * xp.random.rand(3) - GraniciVselennoy)
-    pom[-1].append(RandNormVec() * MagMom)
+#     return mI
 
-    pom[-1].append([0, 0, 0])
-    pom[-1].append([0, 0, 0])
 
-    pom[-1].append([0, 0, 0])
-    pom[-1].append([0, 0, 0])
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def PrimoyMoment(mI, mJ, n, magN):
+#     rIJ = (mI[RadVek] - mJ[RadVek]) + n
+#     magR = math.sqrt(rIJ[0] ** 2 + rIJ[1] ** 2 + rIJ[2] ** 2)
 
-    pom[-1].append([Radiuse, Massa, 0])
-    for i in range(1, CisloChastic):
-        print(
-            "\rСоздано %d из %d частиц"
-            % (len(pom), CisloChastic),
-            end="",
-        )
-        koord = 2 * GraniciVselennoy * xp.random.rand(3) - GraniciVselennoy
-        new_koord = True
-        while new_koord:
-            for x in pom:
-                if xp.absolute(xp.sqrt(xp.sum(xp.square(x[0]))) - xp.sqrt(xp.sum(xp.square(koord)))) > 2 * (Radiuse + Dlina_PAV):
-                    new_koord = False
-                else:
-                    new_koord = True
-                    koord = 2 * GraniciVselennoy * xp.random.rand(3) - GraniciVselennoy
-        
-        pom.append([])            
-        pom[-1].append(koord)
-        pom[-1].append(RandNormVec() * MagMom)
+#     B_I = (
+#         U0
+#         / (4 * xp.pi)
+#         * (Dot(mI[NaprUgl], rIJ) * 3 * rIJ / (magR) ** 5 - mI[NaprUgl] / (magR) ** 3)
+#     )
+#     mI[VekMomentov] += Cross(mJ[NaprUgl], B_I)
 
-        pom[-1].append([0, 0, 0])
-        pom[-1].append([0, 0, 0])
+#     return mI
 
-        pom[-1].append([0, 0, 0])
-        pom[-1].append([0, 0, 0])
 
-        pom[-1].append([Radiuse, Massa, 0])
+# # https://www.desmos.com/calculator/bhjmf8p0pf
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def Kinematika(C):
+#     pom = StahostSmeshLineynoe(C[ParamCastic][R_Chastici])
+#     C[VecSkor] = C[RadVek]
+#     C[RadVek] = (
+#         C[RadVek]
+#         + C[VekSil] / (6.0 * xp.pi * C[ParamCastic][R_Chastici] * Vyazkost) * delta_T
+#         + pom
+#     )
+#     C[VecSkor] = (C[RadVek] - C[VecSkor]) / delta_T
 
-    return xp.array(pom)
+#     pom = StahostSmeshUglovoe(C[ParamCastic][R_Chastici])
+#     C[VekVrash] = C[NaprUgl]
+#     DeltaAlfa = (
+#         C[VekMomentov]
+#         / (8.0 * xp.pi * C[ParamCastic][R_Chastici] ** 3 * Vyazkost)
+#         * delta_T
+#         + pom
+#     )  # xp.linalg.norm(DeltaAlfa) #
+#     buf = math.sqrt(DeltaAlfa[0] ** 2 + DeltaAlfa[1] ** 2 + DeltaAlfa[2] ** 2)
+#     C[NaprUgl] = RotatinVec(C[NaprUgl], DeltaAlfa, buf)
+#     C[VekVrash] = (C[NaprUgl] - C[VekVrash]) / delta_T
+
+#     C = PorvrkaGrani(C)
+#     C[VekSil] = xp.zeros(3)
+#     C[VekMomentov] = xp.zeros(3)
+
+#     return C
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def PorvrkaGrani(mass):
+#     if mass[0][0] > GraniciVselennoy:
+#         mass[0] = xp.array([mass[0][0] - 2 * GraniciVselennoy, mass[0][1], mass[0][2]])
+#     elif mass[0][0] < -GraniciVselennoy:
+#         mass[0] = xp.array([mass[0][0] + 2 * GraniciVselennoy, mass[0][1], mass[0][2]])
+
+#     if mass[0][1] > GraniciVselennoy:
+#         mass[0] = xp.array([mass[0][0], mass[0][1] - 2 * GraniciVselennoy, mass[0][2]])
+#     elif mass[0][1] < -GraniciVselennoy:
+#         mass[0] = xp.array([mass[0][0], mass[0][1] + 2 * GraniciVselennoy, mass[0][2]])
+
+#     if mass[0][2] > GraniciVselennoy:
+#         mass[0] = xp.array([mass[0][0], mass[0][1], mass[0][2] - 2 * GraniciVselennoy])
+#     elif mass[0][2] < -GraniciVselennoy:
+#         mass[0] = xp.array([mass[0][0], mass[0][1], mass[0][2] + 2 * GraniciVselennoy])
+
+#     return mass
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def StahostSmeshLineynoe(Radiuse):
+#     difuz = kT / (6.0 * xp.pi * Radiuse * Vyazkost)
+
+#     return RandNormVec() * ((2 * difuz * delta_T) ** 0.5 * gauss(0, 1))
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def StahostSmeshUglovoe(Radiuse):
+#     difuz = kT / (8.0 * xp.pi * Radiuse ** 3 * Vyazkost)
+
+#     return RandNormVec() * ((2 * difuz * delta_T) ** 0.5 * gauss(0, 1))
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def GeneralLoop(mass):
+#     for x in range(len(mass)):
+#         mass[x] = Kinematika(mass[x])
+
+#     return mass
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def MathKernel(mass, N):
+#     for i in range(len(mass)):
+#         mass[i] = VneshPole(mass[i], N)
+#         for j in range(len(mass)):
+#             mass[i] = SteerOttalk(mass[i], mass[j])
+
+#             PredelSumm = 3
+#             for X in range(-PredelSumm, PredelSumm + 1):
+#                 pom1 = int(math.sqrt(PredelSumm ** 2 - X ** 2))
+#                 for Y in range(-pom1, pom1 + 1):
+#                     pom2 = int(math.sqrt(PredelSumm ** 2 - X ** 2 - Y ** 2))
+#                     for Z in range(-pom2, pom2 + 1):
+#                         n = xp.array(
+#                             [
+#                                 X * GraniciVselennoy * 2,
+#                                 Y * GraniciVselennoy * 2,
+#                                 Z * GraniciVselennoy * 2,
+#                             ]
+#                         )
+#                         magN = math.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
+
+#                         if j != i:
+#                             mass[i] = PrimoySila(mass[i], mass[j], n, magN)
+#                             mass[i] = PrimoyMoment(mass[i], mass[j], n, magN)
+
+#     return mass
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def Dot(a, b):
+#     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def RandNormVec():
+#     a1 = 1 - 2 * random()
+#     a2 = 1 - 2 * random()
+#     a3 = 1 - 2 * random()
+#     sq = math.sqrt(a1 ** 2 + a2 ** 2 + a3 ** 2)
+
+#     return xp.array([a1 / sq, a2 / sq, a3 / sq])
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def Culculete(pom):
+#     return xp.sqrt(xp.sum(xp.square(xp.copy(pom.reshape(len(pom), 7 * 3)[:, NaprUgl * 3:NaprUgl * 3 + 3])), axis = 1)) * MagMom
+
+
+# @jit(fastmath = True, nopython = True, parallel = True)
+# def RotatinVec(vec, axis, ugol):
+#     nK = math.sqrt(axis[0] ** 2 + axis[2] ** 2 + axis[1] ** 2)
+#     axis[0], axis[1], axis[2] = axis[0] / nK, axis[1] / nK, axis[2] / nK
+#     a = math.cos(ugol / 2.0)
+#     sinKoef = math.sin(ugol / 2.0)
+#     b, c, d = -axis[0] * sinKoef, -axis[1] * sinKoef, -axis[2] * sinKoef
+#     aa, bb, cc, dd = a * a, b * b, c * c, d * d
+#     bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+#     q1, q2, q3 = aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)
+#     q4, q5, q6 = 2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)
+#     q7, q8, q9 = 2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc
+#     p0 = q1 * vec[0] + q2 * vec[1] + q3 * vec[2]
+#     p1 = q4 * vec[0] + q5 * vec[1] + q6 * vec[2]
+#     p2 = q7 * vec[0] + q8 * vec[1] + q9 * vec[2]
+#     return xp.array([p0, p1, p2])
